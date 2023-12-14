876b37fb94686d249e2aa0dd57ea297e9f4c8430
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-12-13 18:07:18 -0600

    adding solution for day13

---
+++ day13/solution.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+VERTICAL_FACTOR = 100
+
+
+def compute_symmetry(block: np.ndarray, allowable_error: int = 0):
+    current_line_index = 1
+
+    while current_line_index < len(block):
+        top_half = block[:current_line_index]
+        bottom_half = block[current_line_index:]
+
+        # Extend boundaries so we have matching number of rows/cols on either side of a dividing line
+        if len(top_half) > len(bottom_half):
+            top_half = top_half[len(top_half) - len(bottom_half):]
+        elif len(bottom_half) > len(top_half):
+            bottom_half = bottom_half[: len(top_half)]
+
+        flipped_bottom_half = np.flip(bottom_half, axis=0)
+
+        # this is exactly equal because the problem said so, applying LTE or GTE will not work
+        # the flip could happen with either side
+        if np.count_nonzero(top_half != flipped_bottom_half) == allowable_error:
+            return current_line_index
+
+        current_line_index += 1
+
+    return False
+
+
+def calculate_max_symmetry(blocks: list[list[str]], allowable_error: int = 0):
+    max_symmetry = 0
+
+    for block in blocks:
+        char_lists = [list(line) for line in block]
+        numpy_lined_block = np.array(char_lists)
+        transposed_block = numpy_lined_block.T
+
+        computed_symmetry = compute_symmetry(transposed_block, allowable_error)
+
+        if computed_symmetry:
+            max_symmetry += computed_symmetry
+        else:
+            max_symmetry += VERTICAL_FACTOR * \
+                compute_symmetry(numpy_lined_block, allowable_error)
+
+    return max_symmetry
+
+
+if __name__ == '__main__':
+    lines = open("text.txt").read().split("\n\n")
+    blocks = [line.splitlines() for line in lines]
+
+    print(f"Part 1: {calculate_max_symmetry(blocks)}")
+    print(f"Part 2: {calculate_max_symmetry(blocks, 1)}")
