5a7ff545ad2594bc128127ac811e3e75938abbdc
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-12-16 17:29:18 -0600

    commit missed somehow

--- day10/solution.py
+++ day10/solution.py
@@ -7,6 +7,19 @@ UP = (0, 1)
 
 CARDINAL_DIRECTIONS = [LEFT, RIGHT, DOWN, UP]
 
+VALID_MOVES = {
+    "|": [DOWN, UP],
+    "-": [LEFT, RIGHT],
+    "L": [DOWN, RIGHT],
+    "J": [DOWN, LEFT],
+    "7": [LEFT, UP],
+    "F": [RIGHT, UP],
+}
+
+
+BOTTOM_LEFT = "L"
+TOP_RIGHT = "7"
+
 
 def read_lines_of_file(file) -> list[str]:
     """
@@ -67,16 +80,7 @@ def get_max_distance_of_path(grid_path: Queue, grid: list[str]) -> int:
     return max(get_distances_from_start(grid_path, grid).values())
 
 
-def part_one(lines: list[str]) -> int:
-    valid_moves = {
-        "|": [DOWN, UP],
-        "-": [LEFT, RIGHT],
-        "L": [DOWN, RIGHT],
-        "J": [DOWN, LEFT],
-        "7": [LEFT, UP],
-        "F": [RIGHT, UP],
-    }
-
+def get_grid_path(lines: list[str]):
     grid_path = Queue()
 
     start_x, start_y = find_start_node(lines)
@@ -87,40 +91,58 @@ def part_one(lines: list[str]) -> int:
         possible_next_y = current_y + dy
         next_char = get_grid_char(lines, possible_next_x, possible_next_y)
 
-        if next_char in valid_moves.keys():
-            for next_char_dx, next_char_dy in valid_moves[next_char]:
+        if next_char in VALID_MOVES.keys():
+            for next_char_dx, next_char_dy in VALID_MOVES[next_char]:
                 pathable_x = current_x - next_char_dx == possible_next_x
                 pathable_y = current_y - next_char_dy == possible_next_y
 
                 if pathable_x and pathable_y:
                     grid_path.put((1, (possible_next_x, possible_next_y)))
 
-    return get_max_distance_of_path(grid_path, lines)
+    return grid_path
+
+
+def part_one(lines: list[str]) -> int:
+    return get_max_distance_of_path(get_grid_path(lines), lines)
+
+
+def part_two(lines: list[str]) -> int:
+    grid_width = len(lines[0])
+    grid_height = len(lines)
+
+    path_nodes = get_distances_from_start(get_grid_path(lines), lines)
+
+    nodes_inside_loop = 0
+    for y_index, line in enumerate(lines):
+        for x_index, char in enumerate(line):
+            if (x_index, y_index) in path_nodes:
+                continue
+
+            times_crossed_path = 0
+            current_sub_x, current_sub_y = x_index, y_index
+
+            # probably not efficient but we're going to check the whole sub-square
+            while current_sub_x < grid_width and current_sub_y < grid_height:
+                current_sub_char = get_grid_char(
+                    lines, current_sub_x, current_sub_y)
+
+                current_node = (current_sub_x, current_sub_y)
+
+                if current_node in path_nodes and current_sub_char != BOTTOM_LEFT and current_sub_char != TOP_RIGHT:
+                    times_crossed_path += 1
+
+                current_sub_x += 1
+                current_sub_y += 1
+
+            # this is a ray tracing technique, odd vs even dictates if a node is inside or outside a polygon
+            if times_crossed_path % 2 == 1:
+                nodes_inside_loop += 1
+
+    return nodes_inside_loop
 
 
 if __name__ == '__main__':
     lines = read_lines_of_file("text.txt")
 
     print(f"Part one: {part_one(lines)}")
-
-    w = len(lines[0])
-    h = len(lines)
-
-    inside_count = 0
-    for y, line in enumerate(m):
-        for x, c in enumerate(line):
-            if (x, y) in dists:
-                continue
-
-            crosses = 0
-            x2, y2 = x, y
-
-            while x2 < w and y2 < h:
-                c2 = lines[y2][x2]
-                if (x2, y2) in dists and c2 != "L" and c2 != "7":
-                    crosses += 1
-                x2 += 1
-                y2 += 1
-
-            if crosses % 2 == 1:
-                inside_count += 1
+    print(f"Part two: {part_two(lines)}")
