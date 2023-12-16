a6c1217ba78781261b66ab2cdd121222628a9d24
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-12-16 15:37:21 -0600

    add part 2 solution with comments

--- day16/solution.py
+++ day16/solution.py
@@ -30,12 +30,11 @@ def x_y_in_bounds(grid: list[str], x: int, y: int) -> bool:
     return x_valid and y_valid
 
 
-def part_one(lines: list[str]) -> int:
+def count_energized_tiles(lines: list[str], starting_tile: tuple[int, int, int, int]) -> int:
     beam_queue = Queue()
 
-    # start at top left traveling right
     # x, y, dx, dy
-    beam_queue.put((0, 0, 1, 0))
+    beam_queue.put(starting_tile)
 
     seen_nodes = []
 
@@ -115,9 +114,57 @@ def part_one(lines: list[str]) -> int:
     return len(unique_seen_nodes)
 
 
+def part_one(lines: list[str], example_lines: list[str]) -> None:
+    top_left_moving_right = (0, 0, 1, 0)
+
+    print(
+        f"Part one example: {count_energized_tiles(example_lines, top_left_moving_right)}")
+    print(f"Part one: {count_energized_tiles(lines, top_left_moving_right)}")
+
+
+def compute_maximum_energeized_tiles(lines: list[str]) -> int:
+    x_len = len(lines[0])
+    y_len = len(lines)
+
+    starting_tiles = []
+
+    # top going downward
+    for x in range(0, x_len):
+        starting_tiles.append((x, 0, 0, 1))
+
+    # bottom going upward
+    for x in range(0, x_len):
+        starting_tiles.append((x, y_len - 1, 0, -1))
+
+    # Nathan comment: I actually got the right answer to my problem only generating
+    # the top and bottom starting points, left and right were not computed but I still got 7831, the solution to my part 2
+
+    # left going right
+    for y in range(0, y_len):
+        starting_tiles.append((0, y, 1, 0))
+
+    # right going left
+    for y in range(0, y_len):
+        starting_tiles.append((x_len - 1, y, -1, 0))
+
+    max_energized_tiles = 0
+    for starting_tile in starting_tiles:
+        max_energized_tiles = max(
+            max_energized_tiles, count_energized_tiles(lines, starting_tile))
+        pass
+
+    return max_energized_tiles
+
+
+def part_two(lines: list[str], example_lines: list[str]) -> None:
+    print(
+        f"Part two example: {compute_maximum_energeized_tiles(example_lines)}")
+    print(f"Part two: {compute_maximum_energeized_tiles(lines)}")
+
+
 if __name__ == '__main__':
     lines = read_lines_of_file('text.txt')
     example_lines = read_lines_of_file('example_text.txt')
 
-    print(f"Part one example: {part_one(example_lines)}")
-    print(f"Part one: {part_one(lines)}")
+    part_one(lines, example_lines)
+    part_two(lines, example_lines)
