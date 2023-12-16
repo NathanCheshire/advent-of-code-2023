c27c5e79cb6ff8ef23c16f907520eb20cf41b12d
Nathan Cheshire <nathan.vincent.2.718@gmail.com>
2023-12-16 15:22:07 -0600

    fix bug in day16 part one code

--- day16/solution.py
+++ day16/solution.py
@@ -24,6 +24,12 @@ def get_at_grid_position(grid: list[str], x: int, y: int) -> str:
     return grid[y][x]
 
 
+def x_y_in_bounds(grid: list[str], x: int, y: int) -> bool:
+    x_valid = x < len(grid[0]) and x >= 0
+    y_valid = y < len(grid) and y >= 0
+    return x_valid and y_valid
+
+
 def part_one(lines: list[str]) -> int:
     beam_queue = Queue()
 
@@ -36,27 +42,19 @@ def part_one(lines: list[str]) -> int:
     while not beam_queue.empty():
         current_beam = beam_queue.get()
 
-        current_x = current_beam[0]
-        current_y = current_beam[1]
-        delta_x = current_beam[2]
-        delta_y = current_beam[3]
+        current_x, current_y, delta_x, delta_y = current_beam
 
-        # skip node if already processed
         if current_beam in seen_nodes:
             continue
         seen_nodes.append(current_beam)
 
         current_char = get_at_grid_position(lines, current_x, current_y)
 
-        if is_empty_space(current_char):
-            print('empty space', current_x, current_y, delta_x, delta_y)
-            # position plus velocity for next
+        if is_empty_space(current_char) or (current_char == '-' and delta_x in [-1, 1]) or (current_char == '|' and delta_y in [-1, 1]):
             next_x = current_x + delta_x
             next_y = current_y + delta_y
 
-            x_valid = next_x < len(lines[0]) and next_x >= 0
-            y_valid = next_y < len(lines) and next_y >= 0
-            if x_valid and y_valid:
+            if x_y_in_bounds(lines, next_x, next_y):
                 beam_queue.put((next_x, next_y, delta_x, delta_y))
         elif is_mirror(current_char):
             left = current_x - 1
@@ -65,7 +63,6 @@ def part_one(lines: list[str]) -> int:
             bottom = current_y + 1
 
             if current_char == '/':
-                print('/ mirror', current_x, current_y, delta_x, delta_y)
                 # if coming from top, add left char, going left
                 if delta_y == 1 and left >= 0:
                     beam_queue.put((left, current_y, -1, 0))
@@ -80,7 +77,6 @@ def part_one(lines: list[str]) -> int:
                     beam_queue.put((right, current_y, 1, 0))
 
             elif current_char == '\\':
-                print('\\ mirror', current_x, current_y, delta_x, delta_y)
                 # if coming from top, add right char, going right
                 if delta_y == 1 and right < len(lines[0]):
                     beam_queue.put((right, current_y, 1, 0))
@@ -96,45 +92,21 @@ def part_one(lines: list[str]) -> int:
 
         elif is_splitter(current_char):
             if current_char == '-':
-                if delta_x in [-1, 1]:
-                    print('splitter carrying straight -',
-                          current_x, current_y, delta_x, delta_y)
-                    next_x = current_x + delta_x
-                    next_y = current_y + delta_y
+                left_x = current_x - 1
+                right_x = current_x + 1
 
-                    x_valid = next_x < len(lines[0]) and next_x >= 0
-                    y_valid = next_y < len(lines) and next_y >= 0
-                    if x_valid and y_valid:
-                        beam_queue.put((next_x, next_y, delta_x, delta_y))
-                else:
-                    print('splitter -', current_x, current_y, delta_x, delta_y)
-                    left_x = current_x - 1
-                    right_x = current_x + 1
-
-                    if left_x >= 0:
-                        beam_queue.put((left_x, current_y, -1, 0))
-                    if right_x < len(lines[0]):
-                        beam_queue.put((right_x, current_y, 1, 0))
+                if left_x >= 0:
+                    beam_queue.put((left_x, current_y, -1, 0))
+                if right_x < len(lines[0]):
+                    beam_queue.put((right_x, current_y, 1, 0))
             elif current_char == '|':
-                if delta_y in [-1, 1]:
-                    print('splitter carrying straight |',
-                          current_x, current_y, delta_x, delta_y)
-                    next_x = current_x + delta_x
-                    next_y = current_y + delta_y
+                above_y = current_y - 1
+                below_y = current_y + 1
 
-                    x_valid = next_x < len(lines[0]) and next_x >= 0
-                    y_valid = next_y < len(lines) and next_y >= 0
-                    if x_valid and y_valid:
-                        beam_queue.put((next_x, next_y, delta_x, delta_y))
-                else:
-                    print('splitter |', current_x, current_y, delta_x, delta_y)
-                    above_y = current_y - 1
-                    below_y = current_y + 1
-
-                    if above_y >= 0:
-                        beam_queue.put((current_x, below_y, 0, -1))
-                    if below_y < len(lines):
-                        beam_queue.put((current_x, below_y, 0, 1))
+                if above_y >= 0:
+                    beam_queue.put((current_x, above_y, 0, -1))
+                if below_y < len(lines):
+                    beam_queue.put((current_x, below_y, 0, 1))
 
     unique_seen_nodes = []
     for seen_node in seen_nodes:
@@ -143,16 +115,9 @@ def part_one(lines: list[str]) -> int:
     return len(unique_seen_nodes)
 
 
-def part_two(lines: list[str]) -> int:
-    pass
-
-
 if __name__ == '__main__':
-    example_lines = read_lines_of_file('example_text.txt')
     lines = read_lines_of_file('text.txt')
+    example_lines = read_lines_of_file('example_text.txt')
 
     print(f"Part one example: {part_one(example_lines)}")
     print(f"Part one: {part_one(lines)}")
-
-    print(f"Part two example: {part_two(example_lines)}")
-    # print(f"Part two: {part_two(lines)}")
